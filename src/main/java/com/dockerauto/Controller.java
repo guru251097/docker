5607f38b490373df38f38ac579f7ee7951ad30be
guru251097 <guruvarthan2510@gmail.com>
2024-03-15 15:20:28 +0530

    first auto devops program

---
+++ src/main/java/com/dockerauto/Controller.java
@@ -0,0 +1,16 @@
+package com.dockerauto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping(value="getName")
+	public String getName() {
+		return "VALVAE MAAYAM";
+	}
+	
+	
+
+}
